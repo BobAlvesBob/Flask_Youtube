a7670a112c563094aaa77cdb6f6d2620a5fee3d7
Robson Alves <bobpitaia@gmail.com>
2024-05-10 03:18:53 -0300

    Handling Delete/Put/Get Requests

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 from flask_restful.reqparse import RequestParser
 
 app = Flask(__name__)
@@ -15,16 +15,29 @@ video_put_args.add_argument("likes", type=int, help="Likes on the video",
 
 videos = {}
 
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video doesn't exist...")
+
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(409, message="Video already exists with that ID...")
 
 class Video(Resource):
     def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
         return videos[video_id]
 
+
     def put(self, video_id):
+        abort_if_video_exists(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
-
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist()
+        del videos[video_id]
+        return '',204
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -1,9 +1,18 @@
 import requests
 
 BASE = "http://127.0.0.1:5000/"
-response = requests.put(BASE + "video/1", {"likes": 10})
-print(response.json())
+data = [{"likes": 10, "name": "Kung Fury", "views": 100000, },
+        {"likes": 10, "name": "Highlights Palmeiras 2020", "views": 80000, },
+        {"likes": 35, "name": "Dragon Ball The Movie", "views": 100000, },
+        ]
+for i in range(0, len(data)):
+    response = requests.put(BASE + "video/" + str(i), json=data[i])
+    print(response.json())
 
-response = requests.get(BASE + "video/1")
+input()
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
 
