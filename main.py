e9be2942f59c5cb13c3166e1ccb8a41c988a14ee
Robson Alves <bobpitaia@gmail.com>
2024-05-11 01:08:58 -0300

    Entries to the Database added

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
 
 class VideoModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,31 +17,37 @@ class VideoModel(db.Model):
     def __repr__(self):
         return f"Video(name={self.name}, views={self.views}, likes={self.likes})"
 
-db.create_all()
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Video name", required=True)
 video_put_args.add_argument("views", type=int, help="Views", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes", required=True)
 
-def abort_if_video_exists(video_id):
-    if VideoModel.query.filter_by(id=video_id).first():
-        abort(409, message="Video already exists with that ID...")
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
+
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        video = VideoModel.query.filter_by(id=video_id).first()
-        if not video:
-            abort(404, message="Video doesn't exist...")
-        return video.__dict__, 200
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result: abort(404, message="Video not found")
+        return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        abort_if_video_exists(video_id)
         args = video_put_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video already exists")
         video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
         db.session.add(video)
         db.session.commit()
-        return video.__dict__, 201
+        return video, 201
 
     def delete(self, video_id):
         video = VideoModel.query.filter_by(id=video_id).first()
@@ -49,6 +56,7 @@ class Video(Resource):
         db.session.delete(video)
         db.session.commit()
         return '', 204
+
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 import requests
 
 BASE = "http://127.0.0.1:5000/"
-data = [{"likes": 10, "name": "Kung Fury", "views": 100000, },
-        {"likes": 10, "name": "Highlights Palmeiras 2020", "views": 80000, },
-        {"likes": 35, "name": "Dragon Ball The Movie", "views": 100000, },
-        ]
-for i in range(0, len(data)):
+data = [{"likes": 10, "name": "Kung Fury", "views": 100000},
+        {"likes": 10, "name": "Highlights Palmeiras 2020", "views": 80000},
+        {"likes": 35, "name": "Dragon Ball The Movie", "views": 2000}]
+
+for i in range(len(data)):
     response = requests.put(BASE + "video/" + str(i), json=data[i])
     print(response.json())
 
-input()
-response = requests.delete(BASE + "video/0")
-print(response)
-input()
-response = requests.get(BASE + "video/2")
-print(response.json())
+    input()
+    response = requests.get(BASE + "video/6")
+    print(response.json())
 
+
+    # try:
+    #     print(response.json())
+    # except Exception as e:
+    #     print("Error:", e)
